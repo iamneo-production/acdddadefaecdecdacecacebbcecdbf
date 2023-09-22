56a16d0c8356c45ef863f9c8ca8e723ab67d22af
Iamneo <techadmin@iamneo.ai>
2023-09-22 19:39:39 +0000

    Backup Commit

--- cmp.ps1
+++ cmp.ps1
@@ -1,11 +1,9 @@
 $a = Read-Host "enter num1"
 $b = Read-Host "enter num2"
-total = $((a+b))
-
-if("$total" -le 70) {
+$total = $a + $b 
+if($total -le 70) {
     Write-Host "Fail"
 }
-
 else{
     Write-Host "Pass"
 }
